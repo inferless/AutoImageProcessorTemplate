c9281908e3690be87f21f947446d6ae6e9cbfde6
Rajdeep Borgohain <150957746+rbgo404@users.noreply.github.com>
2025-04-01 16:27:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,31 +17,20 @@ class RequestObjects(BaseModel):
 class ResponseObjects(BaseModel):
     outputs: str = Field(default="")
     
-
-
 class InferlessPythonModel:
-
     def initialize(self):
         self.processor = AutoImageProcessor.from_pretrained("histai/hibou-L", trust_remote_code=True)
-        self.model = AutoModel.from_pretrained("histai/hibou-L", trust_remote_code=True)
+        self.model = AutoModel.from_pretrained("histai/hibou-L", trust_remote_code=True).to("cuda")
 
     def infer(self, inputs: RequestObjects) -> ResponseObjects:
         response = requests.get(inputs.image_url)
         image = Image.open(BytesIO(response.content))
-        
-        model_inputs = self.processor(images=image, return_tensors="pt")
-        
-        # Perform inference
+
+        model_inputs = self.processor(images=image, return_tensors="pt").to("cuda")
         model_outputs = self.model(**model_inputs)
-        
-        # Extract last_hidden_state and convert to list
-        if hasattr(model_outputs, "last_hidden_state") and isinstance(model_outputs.last_hidden_state, torch.Tensor):
-            output_list = model_outputs.last_hidden_state.tolist()
-        else:
-            output_list = []  # Handle the case where last_hidden_state is missing
-        
+        output_list = model_outputs.pooler_output.tolist()
+
         return ResponseObjects(outputs=json.dumps(output_list))
 
-    # perform any cleanup activity here
     def finalize(self,args):
         self.pipe = None
